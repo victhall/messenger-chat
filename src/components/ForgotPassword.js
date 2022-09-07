37ba0dd5021a31a4bf3244a7532785edb682cba3
victhall <victoriahall.dev@gmail.com>
2022-09-07 14:45:58 -0400

    create forgot password component and add styling

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,34 +1,56 @@
 import { Link } from 'react-router-dom'
-import classes from './Login.module.css'
+import classes from './ForgotPassword.module.css'
 
 export default function ForgotPassword() {
   return (
     <div className={classes['outer-fp__container']}>
 
-<div className={classes.header}>
+      <div className={classes.header}>
 
-<p>MSN Messenger</p>
-<div className={classes.container}>
-  <span className={classes.box}>
-    <span className={classes['box-minimize']}></span>
-  </span>
-  <span className={classes.box}>
-    <span className={classes['box-maximize']}></span>
-  </span>
-  <span className={classes.box}>
-    <span className={`${classes['box-exit']} ${classes['box-exit-right']}`}></span>
-    <span className={`${classes['box-exit']} ${classes['box-exit-left']}`}></span>
-  </span>
-</div>
-</div>
+        <p>MSN Messenger</p>
+        <div className={classes.container}>
+          <span className={classes.box}>
+            <span className={classes['box-minimize']}></span>
+          </span>
+          <span className={classes.box}>
+            <span className={classes['box-maximize']}></span>
+          </span>
+          <span className={classes.box}>
+            <span className={`${classes['box-exit']} ${classes['box-exit-right']}`}></span>
+            <span className={`${classes['box-exit']} ${classes['box-exit-left']}`}></span>
+          </span>
+        </div>
+      </div>
 
-<div className={classes.menu}>
-<p className="menu-file">File</p>
-<p className="menu-contacts">Contacts</p>
-<p className="menu-actions">Actions</p>
-<p className="menu-tools">Tools</p>
-<p className="menu-help">Help</p>
-</div>
+      <div className={classes.menu}>
+        <p className="menu-file">File</p>
+        <p className="menu-contacts">Contacts</p>
+        <p className="menu-actions">Actions</p>
+        <p className="menu-tools">Tools</p>
+        <p className="menu-help">Help</p>
+      </div>
+
+      <div className={classes['inner-fp__container']}>
+        <div className={classes['fp-title']}>
+          <h2>Password Reset Help</h2>
+        </div>
+
+        <form>
+          <div className={classes.inputs}>
+            <label>E-mail address:</label>
+            <input type="email" />
+            <div className={classes['reset-password']}>
+              <button className={classes['reset-btn']}>Reset Password</button>
+            </div>
+          </div>
+
+        </form>
+        <div className={classes['fp-footer']}>
+          <p>Already have an account?</p>
+          <Link className={classes['back-link']} to="/login">Back to Login</Link>
+        </div>
+
+      </div>
 
     </div>
   )
