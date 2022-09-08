a1a5a3abbec0887f787b1f31969b08e484736809
victhall <victoriahall.dev@gmail.com>
2022-09-08 10:31:58 -0400

    set up auth context

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,28 +1,34 @@
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 import classes from './Signup.module.css'
 
 export default function Signup() {
+const usernameRef = useRef();
+const emailRef = useRef();
+const passwordRef = useRef();
+const confirmPasswordRef = useRef();
+
   return (
     <>
-    <div className={classes['outer-signup__container']}>
-      <div className={classes.header}>
-        <p>Messenger</p>
-        <div className={classes.container}>
-          <span className={classes.box}>
-            <span className={classes['box-minimize']}></span>
-          </span>
-          <span className={classes.box}>
-            <span className={classes['box-maximize']}></span>
-          </span>
-          <span className={classes.box}>
-            <span className={`${classes['box-exit']} ${classes['box-exit-right']}`}></span>
-            <span className={`${classes['box-exit']} ${classes['box-exit-left']}`}></span>
-          </span>
+      <div className={classes['outer-signup__container']}>
+        <div className={classes.header}>
+          <p>Messenger</p>
+          <div className={classes.container}>
+            <span className={classes.box}>
+              <span className={classes['box-minimize']}></span>
+            </span>
+            <span className={classes.box}>
+              <span className={classes['box-maximize']}></span>
+            </span>
+            <span className={classes.box}>
+              <span className={`${classes['box-exit']} ${classes['box-exit-right']}`}></span>
+              <span className={`${classes['box-exit']} ${classes['box-exit-left']}`}></span>
+            </span>
 
+          </div>
         </div>
-      </div>
 
-      <div className={classes.menu}>
+        <div className={classes.menu}>
           <p className="menu-file">File</p>
           <p className="menu-contacts">Contacts</p>
           <p className="menu-actions">Actions</p>
@@ -32,32 +38,51 @@ export default function Signup() {
 
         <div className={classes['inner-signup__container']}>
           <div className={classes['signup-title']}>
-      <h2>Messenger Sign Up</h2>
-</div>
-      <form>
+            <h2>Messenger Sign Up</h2>
+          </div>
+          <form>
 
-        <div className={classes.inputs}>
-            <label>Username:</label>
-            <input type="text"/>
-            <label>E-mail Address:</label>
-            <input type="email"/>
-            <label>Password:</label>
-            <input type="password"/>
-            <label>Confirm Password:</label>
-            <input type="password"/>
+            <div className={classes.inputs}>
+              <label>Username:</label>
+              <input
+                type="text"
+                ref={usernameRef}
+                required
+              />
 
-          <div className={classes['signup']}>
-            <button className={classes['signup-btn']}>Sign Up</button>
+              <label>E-mail address:</label>
+              <input
+                type="email"
+                ref={emailRef}
+                required
+              />
+
+              <label>Password:</label>
+              <input
+                type="password"
+                ref={passwordRef}
+                required
+              />
+
+              <label>Confirm password:</label>
+              <input
+                type="password"
+                ref={confirmPasswordRef}
+                required
+              />
+
+              <div className={classes['signup']}>
+                <button className={classes['signup-btn']}>Sign Up</button>
+              </div>
+
             </div>
 
+          </form>
+          <div className={classes['signup-footer']}>
+            <p>Already have an account?</p>
+            <Link to="/login">Log in</Link>
+          </div>
         </div>
-
-      </form>
-      <div className={classes['signup-footer']}>
-        <p>Already have an account?</p>
-        <Link to="/login">Log in</Link>
-      </div>
-      </div>
 
       </div>
     </>
